2e887d1946e4ba6ffe7b834b2a49828dac53bb00
Marginean Sebastian <margineansebastian18@gmail.com>
2023-04-10 13:35:07 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+  openWeatherMap: {
+    BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECRET_KEY: "36c0463a162602200dd490dfc5ad62e1",
+  },
+};
+module.exports = constants;
